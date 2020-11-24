0826dcfc5e8eb737560795267cca1620129a5066
Dominique Martinet <asmadeus@codewreck.org>
2020-11-24 19:12:25 +0100

    resnap: allow skipping some ssh

--- host/resnap.sh
+++ host/resnap.sh
@@ -49,6 +49,15 @@ while (( "$#" )); do
       ADDRESS=$2
       shift 2
       ;;
+    -f)
+	pid=$2
+	skip_bytes_hex=$3
+	shift 3
+	;;
+    -F)
+	PRINTFAST=1
+	shift
+	;;
     -i)
         SSH_OPT="-i $2"
         shift 2
@@ -81,6 +90,8 @@ then
     echo "resnap: Error: Command 'ffmpeg' not found. FFmpeg not installed"
     exit 1
 fi
+ 
+SSH_CMD="ssh $SSH_OPT $ADDRESS"
 
 # Check if output file already exists
 if [ -f $OUTPUT ]; then
@@ -99,12 +110,12 @@ bytes_per_pixel=1
 window_bytes="$((width * height * bytes_per_pixel))"
 
 # find xochitl's process
-pid="$(ssh "$ADDRESS" pidof xochitl)"
+[[ -n "$pid" ]] || pid="$($SSH_CMD pidof xochitl)"
 
 # find framebuffer location in memory
 # it is actually the map allocated _after_ the fb0 mmap
 read_address="grep -A1 '/dev/fb0' /proc/$pid/maps | tail -n1 | sed 's/-.*$//'"
-skip_bytes_hex="$(ssh "$ADDRESS" "$read_address")"
+[[ -n "$skip_bytes_hex" ]] || skip_bytes_hex="$($SSH_CMD "$read_address")"
 skip_bytes="$((0x$skip_bytes_hex + 8))"
 
 # carve the framebuffer out of the process memory
@@ -113,9 +124,11 @@ window_start_blocks="$((skip_bytes / page_size))"
 window_offset="$((skip_bytes % page_size))"
 window_length_blocks="$((window_bytes / page_size + 1))"
 
+[[ -n "$PRINTFAST" ]] && echo $pid $skip_bytes_hex
+
 # Using dd with bs=1 is too slow, so we first carve out the pages our desired
 # bytes are located in, and then we trim the resulting data with what we need.
-ssh "$ADDRESS" "dd if=/proc/$pid/mem bs=$page_size skip=$window_start_blocks count=$window_length_blocks 2>/dev/null |
+$SSH_CMD "dd if=/proc/$pid/mem bs=$page_size skip=$window_start_blocks count=$window_length_blocks 2>/dev/null |
   /opt/bin/zstd" | zstd -d |
   tail -c+$window_offset | head -c $window_bytes |
   ffmpeg -vcodec rawvideo \
